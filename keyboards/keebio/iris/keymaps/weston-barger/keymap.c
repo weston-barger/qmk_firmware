8c1a4836f822bef4060900403dfe7c5045eb47c5
Weston Barger <weston.barger@gmail.com>
2024-04-06 18:56:11 -0700

    v1

--- keyboards/keebio/iris/keymaps/weston-barger/keymap.c
+++ keyboards/keebio/iris/keymaps/weston-barger/keymap.c
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
+#include "print.h"
 
 #define _DEFAULT 0
 #define _VIM 1
@@ -35,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, EOL, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, BOL, XXXXXXX,
      XXXXXXX, XXXXXXX, XXXXXXX, EOW, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, TO(_DEFAULT), XXXXXXX, XXXXXXX, XXXXXXX,
      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
-     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TG(_VISUAL), BOW,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+     _______, XXXXXXX, XXXXXXX, XXXXXXX, TG(_VISUAL), BOW, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
                                     XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX
   ),
 
@@ -49,9 +50,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_SPECIAL] = LAYOUT(
      S(KC_GRV), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-     XXXXXXX, XXXXXXX, XXXXXXX, S(KC_LBRC), S(KC_LBRC), TERM,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+     XXXXXXX, XXXXXXX, XXXXXXX, S(KC_LBRC), S(KC_RBRC), TERM,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
      XXXXXXX, XXXXXXX, XXXXXXX, S(KC_9), S(KC_0), XXXXXXX,                            XXXXXXX, KC_MINS, S(KC_MINS), KC_EQL, S(KC_EQL), KC_GRV,
-     KC_LSFT, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, S(KC_COMM), S(KC_DOT), XXXXXXX, KC_RSFT,
+     KC_LSFT, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX,          QK_BOOT, XXXXXXX, XXXXXXX, S(KC_COMM), S(KC_DOT), XXXXXXX, KC_RSFT,
                                     KC_LGUI, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX
   ),
 
@@ -59,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            TAB_LEFT, TAB_RIGHT, XXXXXXX, XXXXXXX, XXXXXXX, KC_DEL,
      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, XXXXXXX, XXXXXXX,
-     KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT,
+     KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT,
                                     KC_LGUI, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, KC_LALT
   ),
 
@@ -91,7 +92,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 SEND_STRING(SS_DOWN(X_LALT) SS_DOWN(X_LEFT) SS_UP(X_LEFT) SS_UP(X_LALT));
                 return false;
             case EOL:
-                if (get_mods() & MOD_MASK_SHIFT) {
+                if (get_mods() == MOD_MASK_SHIFT) {
                     SEND_STRING(SS_DOWN(X_LGUI) SS_DOWN(X_RIGHT) SS_UP(X_RIGHT) SS_UP(X_LGUI));
                     return false;
                 }
@@ -109,20 +110,30 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 };
 
 void keyboard_post_init_user(void) {
+    debug_enable=true;
+    // debug_matrix=true;
     set_single_persistent_default_layer(_DEFAULT);
     rgb_matrix_disable();
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
+    uprintf("state: %d\n", state);
+    uprintf("statecmp: %d\n", layer_state_cmp(state, _DEFAULT));
+    uprintf("statecmp: %d\n", layer_state_cmp(state, _VIM));
+    uprintf("statecmp: %d\n", layer_state_cmp(state, _NORMAL));
+    
+    
     if (layer_state_cmp(state, _DEFAULT)) {
         rgb_matrix_disable();
         return state;
-    }
+    } 
     rgb_matrix_enable();
-    if (layer_state_cmp(state, _VIM)) {
-        rgb_matrix_set_color_all(255, 0, 0);
+    rgb_matrix_set_flags(LED_FLAG_ALL);
+    rgb_matrix_mode(RGB_MATRIX_SOLID_COLOR); 
+    if (layer_state_cmp(state, _VIM)){    
+        rgb_matrix_sethsv(HSV_RED);
     } else if (layer_state_cmp(state, _NORMAL)) {
-        rgb_matrix_set_color_all(0, 255, 0);
+        rgb_matrix_sethsv(HSV_GREEN);
     }
     return state;
 }
